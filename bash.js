b5dd460599db4afc665e858e4b3dd63550564c05
Colin Forbes <cjforbes32@gmail.com>
2022-01-14 16:30:47 -0500

    Added ls module and cleaned up code in all other files

--- bash.js
+++ bash.js
@@ -1,20 +1,15 @@
-
 const pwd = require("./pwd");
 const ls = require("./ls");
+
 process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
-    pwd;
-
-  }
-  else if(cmd === "ls") {
-    ls
-  }
-  else {
+    pwd();
+  } else if (cmd === "ls") {
+    ls();
+  } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
   }
 });
-
-// console.log(process.cwd());
--- pwd.js
+++ pwd.js
@@ -1,7 +1,4 @@
-const pwd = process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === "pwd") {
-    process.stdout.write(`${process.cwd()}`);
-    process.stdout.write("\nprompt > ");
-  }})
-module.exports = pwd
+module.exports = function () {
+  process.stdout.write(`${process.cwd()}`);
+  process.stdout.write("\nprompt > ");
+};
